add971eb3f87ba3f2bf0e3846d072d3c0fa421cb
anshsgit <anshsmail738@gmail.com>
2025-04-22 04:51:55 +0530

    update authentication file

--- backend/authentication/index.js
+++ backend/authentication/index.js
@@ -2,15 +2,24 @@ const jwt = require('jsonwebtoken');
 const passkey = "aabra-ka-daabra";
 
 function userAuthorization(req, res, next) {
-    let auth = req.headers.authorization;
-    let token = auth.split(' ');
-    let verify = jwt.verify(token[1], passkey);
-    if(verify) {
-        req.id = verify._id;
-        next();
-    } else {
-        res.status(400).json({message: "Bad request."});
+    try {
+        let auth = req.headers.authorization;
+        let token = auth.split(' ');
+        console.log(token[1]);
+        let verify = jwt.verify(token[1], passkey);
+        console.log(verify);
+        if(verify) {
+            req.id = verify._id;
+            next();
+        } else {
+            res.status(400).json({message: "Bad request."});
+
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
     }
+    
 }
 
 module.exports = {userAuthorization, passkey};
