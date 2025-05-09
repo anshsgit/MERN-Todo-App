800120b28b1f92606129964a2d0cd4399031d926
anshsgit <anshsmail738@gmail.com>
2025-05-09 19:01:16 +0530

    deleted files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import axios from "axios";
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Todos from './components/Todos';
-import PostTodo from './components/PostTodo';
-import Error from './components/Error';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Todos from './components/Todos/Todos';
+import PostTodo from './components/PostTodo/PostTodo';
+import Error from './components/Error/Error';
 import './App.css'
 
 function App() {
@@ -31,12 +31,19 @@ useEffect(() => {
     <Routes>
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path='/login' element={<Login setTodos={setTodos} setToken={setToken} setError={setError}   style={{marginTop: '50px'}}/>}/>
+
+        <Route path='/login' element={<Login setTodos={setTodos} setToken={setToken} setError={setError}/>}/>
+
         <Route path='/signup' element={<Signup setTodos={setTodos} setToken={setToken} setError={setError} clearTodos={clearTodos}/>} />
+
         <Route path='/todos' element={<Todos todos={todos} setTodos={setTodos} token={token} setError={setError} />} >
+
           <Route path='post' element={<PostTodo setTodos={setTodos} token={token}  setError={setError} />}/>
+
         </Route>
+
         <Route path='/error' element={<Error error={error} />} />
+
       </Route>
     </Routes>
   </BrowserRouter>
--- frontend/src/components/Error.jsx
+++
@@ -1,8 +0,0 @@
-const Error = ({error}) => {
-
-    return <div style={{display: 'flex', justifyContent: 'center', border: '1px solid black', marginTop: '50px'}}>
-    <div>{error} :Kindly try again.</div>
-    </div>
-}
-
-export default Error;
