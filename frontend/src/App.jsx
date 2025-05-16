badeba5490313285e0e457088c1051d7498a8fae
anshsgit <anshsmail738@gmail.com>
2025-05-16 15:28:26 +0530

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
+
+import React, {useState, useEffect, useContext} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import axios from "axios";
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -8,45 +8,35 @@ import Signup from './components/Signup/Signup';
 import Todos from './components/Todos/Todos';
 import PostTodo from './components/PostTodo/PostTodo';
 import Error from './components/Error/Error';
+import ContextProvider, { TodoContext } from './Context/Context';
 import './App.css'
 
+
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [token, setToken] = useState('');
-  const [error, setError] = useState('');
-
-  debugger
-function clearTodos() {
-  setTodos([]);
-}
-
-useEffect(() => {
-  if(!token) {
-    clearTodos();
-  }
-}, [token])
 
   return (
-  <BrowserRouter>
+    <ContextProvider>
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
 
-        <Route path='/login' element={<Login setTodos={setTodos} setToken={setToken} setError={setError}/>}/>
+        <Route path='/login' element={<Login/>}/>
 
-        <Route path='/signup' element={<Signup setTodos={setTodos} setToken={setToken} setError={setError} clearTodos={clearTodos}/>} />
+        <Route path='/signup' element={<Signup/>} />
 
-        <Route path='/todos' element={<Todos todos={todos} setTodos={setTodos} token={token} setError={setError} />} >
+        <Route path='/todos' element={<Todos/>} >
 
-          <Route path='post' element={<PostTodo setTodos={setTodos} token={token}  setError={setError} />}/>
+          <Route path='post' element={<PostTodo/>}/>
 
         </Route>
 
-        <Route path='/error' element={<Error error={error} />} />
+        <Route path='/error' element={<Error/>} />
 
       </Route>
     </Routes>
   </BrowserRouter>
+  </ContextProvider>
   )
 }
 
