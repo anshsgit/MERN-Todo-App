fdacf0aaaf8c764dae3f0bbdda376385ca1e534a
anshsgit <anshsmail738@gmail.com>
2025-05-16 18:18:58 +0530

    fix: [router basename updated]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <ContextProvider>
-    <BrowserRouter>
+    <BrowserRouter basename='/MERN-Todo-App'>
     <Routes>
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
